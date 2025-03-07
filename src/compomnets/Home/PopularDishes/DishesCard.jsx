25ae20f797535ce9cac128438c7481d328e87124
Thetiptop007 <thetiptop007@gmail.com>
2025-03-07 22:10:17 +0530

    Update DishesCard.jsx

--- src/compomnets/Home/PopularDishes/DishesCard.jsx
+++ src/compomnets/Home/PopularDishes/DishesCard.jsx
@@ -39,14 +39,14 @@ function DishesCard({ dish }) {
 
   return (
     <div className='w-full md:w-80 lg:w-96 md:min-h-[350px] lg:min-h-[400px] shadow shadow-stone-200 md:shadow-xl md:shadow-stone-400 border border-stone-200 rounded-t-[50px] md:rounded-t-[70px] lg:rounded-t-[90px] rounded-b-2xl md:rounded-b-3xl lg:rounded-b-4xl p-5 md:p-8 lg:p-10 animate-slide-up'>
-      <div className='flex justify-between items-center min-h-24 md:min-h-28 lg:min-h-32 poppins-medium'>
-        <div>{dish?.rating}</div>
+      <div className='flex justify-center items-center min-h-24 md:min-h-28 lg:min-h-32 poppins-medium'>
+{/*         <div>{dish?.rating}</div> */}
         <div className='relative'>
           <div className='w-24 md:w-32 lg:w-40 aspect-square rounded-full overflow-hidden shadow-xl shadow-stone-400 border-2 md:border-3 lg:border-4 border-stone-400'>
             <img src={dish?.image} alt={dish?.name} className='w-full h-full object-cover' />
           </div>
         </div>
-        <div>{dish?.reviews}K</div>
+{/*         <div>{dish?.reviews}K</div> */}
       </div>
       <div className='text-center mt-5 md:mt-8 lg:mt-10'>
         <div className='text-lg md:text-xl lg:text-2xl poppins-medium'>{dish?.name}</div>
