c4e67d7e1588c6f4931659361dcbe86ab7b3fad6
Thetiptop007 <thetiptop007@gmail.com>
2025-03-11 14:12:09 +0530

    Update Navbar.jsx for message

--- src/compomnets/Navbar.jsx
+++ src/compomnets/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { BsHandbag } from "react-icons/bs";
-import { GiChickenOven } from "react-icons/gi";
 import { Link } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import Cart from './Cart/Cart';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 function Navbar() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -39,12 +38,15 @@ function Navbar() {
         }
 
         const orderDetails = addedDishes.map((dish, index) => {
-            return `${index + 1}️⃣ ${dish.name} - ${dish.quantity} ${dish.quantity > 1 ? 'Pieces' : 'Piece'}`;
+            const quantityDetail = dish.quantity ? `(${dish.quantity})` : '';
+            const plateQuantityDetail = dish.plateQuantity ? `(${dish.plateQuantity})` : '';
+            const priceDetail = `(${dish.price} x ${dish.quantity} = ${dish.price * dish.quantity})`;
+            return `${index + 1}️⃣ ${dish.name} ${plateQuantityDetail} - ${dish.quantity} ${dish.quantity > 1 ? 'Pieces' : 'Piece'} ${priceDetail}`;
         }).join('\n');
 
-        const message = `Hello, I’d like to place an order:\n\n🛒 Order Details:\n${orderDetails}\n\n💰 Total Amount: ₹${totalAmount.toFixed(2)}\n\n📍 Delivery Address: ${address}\n👤 Name: ${name}\n\nHelpline No: +91 9650780199`;
+        const message = `Hello, I’d like to place an order:\n\n🛒 Order Details:\n${orderDetails}\n\n💰 Total Amount: ₹${totalAmount.toFixed(2)}\n\n📍 Delivery Address: ${address}\n👤 Name: ${name}\n\nHelpline No: +91 9060557296`;
 
-        const whatsappUrl = `https://wa.me/7696482938?text=${encodeURIComponent(message)}`;
+        const whatsappUrl = `https://wa.me/9060557296?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, '_blank');
     };
 
@@ -76,29 +78,29 @@ function Navbar() {
                         </div>
                         <div className='mt-8'>
                             <label htmlFor="name" className='block font-medium text-gray-700 poppins-regular'>Name</label>
-                            <input 
-                                type="text" 
-                                id="name" 
-                                name="name" 
-                                value={name} 
-                                onChange={(e) => setName(e.target.value)} 
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
                                 className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm outline-stone-600 outline-1'
                                 placeholder="Enter your name"
                             />
                         </div>
                         <div className='mt-8'>
                             <label htmlFor="address" className='block font-medium text-gray-700 poppins-regular'>Delivery Address</label>
-                            <input 
-                                type="text" 
-                                id="address" 
-                                name="address" 
-                                value={address} 
-                                onChange={(e) => setAddress(e.target.value)} 
+                            <input
+                                type="text"
+                                id="address"
+                                name="address"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
                                 className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm outline-stone-600 outline-1'
                                 placeholder="Enter your delivery address"
                             />
                         </div>
-                        <div className='text-center poppins-medium text-stone-500 mt-5 text-sm'>Free Order Delivery On Order Above <span className='bg-red-200 px-1 rounded-sm border border-red-300'>Rs. 100/-</span></div>
+                        <div className='text-center poppins-medium text-stone-500 mt-5 text-sm'>Free Order Delivery On Above <span className='bg-red-200 px-1 rounded-sm border border-red-300'>Rs. 100/-</span></div>
                         <div className='mt-8 text-white poppins-medium flex justify-between items-center sticky md:static bottom-0 bg-stone-50 md:bg-transparent shadow-lg md:shadow-none shadow-stone-600 border md:border-none border-stone-300 rounded-2xl p-3 md:px-0 text-sm md:text-base'>
                             <div
                                 className='bg-red-400 text-white px-4 md:px-6 rounded-tl-lg rounded-bl-3xl rounded-tr-3xl rounded-br-lg hover:bg-red-500 transition-all duration-300 cursor-pointer py-3'
