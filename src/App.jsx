718df2e4fc6c2df03a4a1a646d5e5f56852ebe50
Naitik2408 <naitikkumar2408@gmail.com>
2026-01-12 00:30:09 +0530

    created the privacy and policy page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 import AppLayout from './layout/AppLayout';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import Orders from './pages/Admin/Orders';
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path='/menu' element={<Menu />} />
+            <Route path='/privacy-policy' element={<PrivacyPolicy />} />
           </Route>
 
           {/* Admin Login Route */}
